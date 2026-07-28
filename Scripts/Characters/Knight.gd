8b8390f583ec650b3ee8407a3d97138d0342c74a
Aaron Mathew <aaron.nadar@hotmail.com>
2026-07-29 01:48:37 +0530

    Making Knight and Slime Enemies!

---
+++ Scripts/Characters/Knight.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var Health: int = 100
+var isAlive: bool = true
+
+@onready var Sprite: AnimatedSprite2D = $Sprite
+
+func _physics_process(delta: float):
+	if isAlive:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			Sprite.play("Jump")
+
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		var direction := Input.get_axis("Left", "Right")
+		if direction:
+			velocity.x = direction * SPEED
+			Sprite.flip_h = true if velocity.x < 0 else false
+			Sprite.play("Walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			Sprite.play("Idle")
+		
+		move_and_slide()
+
+func takeDamage(amount):
+	Health -= amount
+	Sprite.play("Hurt")
+	
+	if Health <= 0:
+		Sprite.play("Death")
+		await Sprite.animation_finished
+		isAlive = false
